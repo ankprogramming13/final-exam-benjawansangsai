94476d02e8b7a156a688f374c838f871fd945fd5
Unknown <benjawan.0052232@gmail.com>
2018-02-22 12:00:29 +0700

    ข้อ 1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-
+        int a,b ,cmd,reusult_1,reusult_2,reusult_3,reusult_4
+        cout<<"welcome to CLI Calculator V1.00"<<endl;
+        cout<<"number 1 : ";
+        cout<<a;
+        cout<<"number 2 : ";
+        cin<<b;
+        do {
+            cout<<"menu\n1.+\n2.-\n3.*\n4./\n5.quit"<<end1;
+        cin<<cmb;
+        if(cmd==1){
+            reusult_1=a+b;
+            cout<<"Result = "<<reusult_1<<end;
+        }clse if (cmd==2){
+              reusult_1=a-b;
+            cout<<"Result = "<<reusult_2<<end;
+        }clse if (cmd==3)
+         reusult_1=a*b;
+            cout<<"Result = "<<reusult_3<<end;
+        }clse if (cmd==4)
+         reusult_1=a/b;
+            cout<<"Result = "<<reusult_4<<endl;
+        }break;)
+        }while (1);
         return 0;
 }
